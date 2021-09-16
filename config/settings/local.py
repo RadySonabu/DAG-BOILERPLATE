248c003b6ab496c03b1451ed81e910d2b94c6893
RadySonabu <40710670+RadySonabu@users.noreply.github.com>
2021-09-17 02:58:36 +0800

    Update local.py

--- config/settings/local.py
+++ config/settings/local.py
@@ -60,7 +60,7 @@ if DEBUG:
     }
 else: 
 #     STATICFILES_DIRS = (os.path.join(os.path.dirname(BASE_DIR), "static"),)
-    STATICFILES_DIRS = os.path.join(BASE_DIR, 'static')
+    STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
     STATIC_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
     STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
