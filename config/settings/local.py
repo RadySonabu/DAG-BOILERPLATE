3776572a6739735ab48319af11d1cba4c2dbb732
RadySonabu <40710670+RadySonabu@users.noreply.github.com>
2021-09-17 02:55:52 +0800

    Update local.py

--- config/settings/local.py
+++ config/settings/local.py
@@ -59,7 +59,9 @@ if DEBUG:
         'INTERCEPT_REDIRECTS': False,
     }
 else: 
-    STATICFILES_DIRS = (os.path.join(os.path.dirname(BASE_DIR), "static"),)
+#     STATICFILES_DIRS = (os.path.join(os.path.dirname(BASE_DIR), "static"),)
+    STATICFILES_DIRS = os.path.dirname(BASE_DIR), "static")
+
 
     STATIC_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
     STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
